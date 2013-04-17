77982ce7a24cf557be057562531f6cf3c994e6c3
Arthur Darcet <arthur.darcet@m4x.org>
2013-04-18 00:37:52 +0200

    Use CherryPy instead of Flask, Python 3 instead of 2, read config

--- harmopy/main.py
+++ harmopy/main.py
@@ -1,32 +1,46 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 import argparse
-import ConfigParser
+import configparser
 import logging
 import time
 
 try:
     from . import rsync
     from . import status
-except ValueError:
+except SystemError:
     import rsync
     import status
 
 
-def main():
+def main(config='harmopy.conf'):
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--debug', action='store_true', help='Log debug messages', default=False)
+    parser.add_argument(
+        '-d', '--debug',
+        action='store_true',
+        help='Log debug messages',
+        default=False
+    )
+    parser.add_argument(
+        '-c', '--config',
+        help='Config file',
+        default=config,
+    )
 
     args = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
 
+    config = configparser.ConfigParser()
+    with open(args.config, 'r') as f:
+        config.readfp(f)
+
     rsyncs = rsync.RsyncManager([])
-    server = status.Status(rsyncs)
+    server = status.StatusThread(args.debug, config, rsyncs)
     server.start()
 
     while True:
         # check runtime
         rsyncs.tick()
-        time.sleep(42)
+        time.sleep(int(config['general']['check_sleep']))
 
 if __name__ == '__main__':
     main()
--- harmopy/status.py
+++ harmopy/status.py
@@ -1,19 +1,37 @@
-import flask
+import cherrypy
 import json
 import threading
 
+# Monkey patch to avoid getting self closing server
+from cherrypy.process import servers
+def fake_wait_for_occupied_port(host, port): return
+servers.wait_for_occupied_port = fake_wait_for_occupied_port
 
-class Status(threading.Thread):
-    def run(self):
-        self.app.run()
 
-    def __init__(self, rsyncs):
-        super(Status, self).__init__()
+class StatusPage(object):
+    def __init__(self, rsyncs, config):
+        self._rsyncs = rsyncs
+        self._config = config
+
+    @cherrypy.expose
+    def status(self):
+        return json.dumps(self._rsyncs.status)
+
+    @cherrypy.expose
+    def config(self, **kwargs):
+        return json.dumps(kwargs)
+        return json.dumps(dict((k, dict(v)) for k,v in dict(self._config).items()))
+
+
+class StatusThread(threading.Thread):
+    def __init__(self, debug, config, rsyncs, *args, **kwargs):
+        super(StatusThread, self).__init__(*args, **kwargs)
+        cherrypy.config.update({
+            'server.socket_host': config['status']['host'],
+            'server.socket_port': int(config['status']['port']),
+        })
+        self.page = StatusPage(rsyncs, config)
         self.daemon = True
-        self.rsyncs = rsyncs
 
-        self.app = flask.Flask(__name__)
-
-        @self.app.route('/status')
-        def status():
-            return json.dumps(self.rsyncs.status)
+    def run(self):
+        cherrypy.quickstart(self.page)
